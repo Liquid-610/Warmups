3bc13d9921e63dec11bb3ed5d3bc8664d8cb4a5d
Liquid-610 <jamesbarnes71@gmail.com>
2014-05-07 09:44:48 -0400

    1

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverse
+{
+    class Program
+    {
+        public static String reverse(string input)
+        {
+            input = input.ToLower();
+            char[] a = input.ToCharArray();
+            Array.Reverse(a);
+
+            for (int i = 1; i <= a.Length; i += 2)
+            {
+                a[i] = (char)(a[i] - 32);  //.ToString().ToUpper()[0];
+ 
+            }        
+
+            return new string(a);
+          
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            String output = reverse(input);
+
+
+
+
+
+
+            Console.Write(output);
+
+            
+        }
+    }
+}
